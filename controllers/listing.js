61ea6c6feadca29ba5a43cf885ee8ff8d4320daf
Nikkath Zabeen <nikkathzabeenshaik@gmail.com>
2025-07-09 13:54:17 +0530

    handles page not found

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,118 +1,117 @@
-const listing=require("../model/listing.js");
+const listing = require("../model/listing.js");
 
-module.exports.index=async (req,res)=>{
-    const allListing = await  listing.find({});
-    res.render("listings/index",{allListing});
+module.exports.index = async (req, res) => {
+  const allListing = await listing.find({});
+  console.log("🎯 All listings:", allListing);
+  res.render("listings/index", { allListing });
 };
 
-module.exports.renderNewForm=(req,res)=>{
-   
-    res.render("listings/new.ejs");
-  };
+module.exports.renderNewForm = (req, res) => {
 
-  module.exports.displayListings = async (req, res) => {
-    const category=req.params.category;
-    const trendingListings = await listing.find({category: category });
+  res.render("listings/new.ejs");
+};
 
-    res.render('listings/icon.ejs', {  allListing:trendingListings });
-  };
-  
-  module.exports.searchListing=async(req,res)=>{
-const location=req.params.location;
-const allListing= await listing.find({location:location});
+module.exports.displayListings = async (req, res) => {
+  const category = req.params.category;
+  const trendingListings = await listing.find({ category: category });
 
-res.render('listings/icon.ejs',{allListing});
-  };
+  res.render('listings/icon.ejs', { allListing: trendingListings });
+};
 
+module.exports.searchListing = async (req, res) => {
+  const location = req.params.location;
+  const allListing = await listing.find({ location: location });
 
-module.exports.showListing=async(req,res)=>{
-    let {id}=req.params;
-    let details= await listing.findById(id).populate({path:"reviews",populate:{path:"author"},}).populate("owner");
-  
-    if(!details)
-    {
-      req.flash("error","Listing you requested for does not exist!");
-      res.redirect("/listings");
-    }
-    
-    res.render("listings/show.ejs",{details});
-  };
-
-
-
-  module.exports.createListing = async (req, res, next) => {
-    try {
-        const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-        const mapToken = process.env.MAP_TOKEN;
-  
-        const geocodingClient = mbxGeocoding({ accessToken: mapToken });
-
-        let response = await geocodingClient.forwardGeocode({
-            query: req.body.listing.location,
-            limit: 1
-        }).send();
-
-        let url = req.file.path;
-        let filename = req.file.filename;
-        let newListing = new listing(req.body.listing);
-        newListing.owner = req.user._id;
-        newListing.image = { url, filename };
-
-        // Assign geometry only if valid
-        newListing.geometry = response.body.features[0].geometry;
-       
-        let savedListing = await newListing.save();
-        console.log(savedListing);
-        
-        
-     
-
-        req.flash("success", "New listing is created");
-        res.redirect("/listings");
-    } catch (err) {
-        console.error(err);
-        req.flash("error", "Failed to create a new listing. Please try again.");
-        res.redirect("/listings/new");
-    }
+  res.render('listings/icon.ejs', { allListing });
 };
 
 
-  module.exports.renderEditForm=async(req,res,next)=>{
-    let {id}=req.params;
-    let details= await listing.findById(id);
-    if(!details)
-        {
-          req.flash("errormsg","Listing you requested for does not exist!");
-          return res.redirect("/listings");
-        }
-        originalImageUrl=details.image.url;
-        originalImageUrl.replace("/upload","/upload/h_300,w_250");
-    res.render("listings/edit.ejs",{details, originalImageUrl});
-};
+module.exports.showListing = async (req, res) => {
+  let { id } = req.params;
+  let details = await listing.findById(id).populate({ path: "reviews", populate: { path: "author" }, }).populate("owner");
 
-module.exports.updateListing=async(req,res,next)=>{
-    if(!req.body.listing){
-        throw new expressError(400,"Send valid data for listing");
-    }
-    let {id}=req.params;
-   let Listing=  await listing.findByIdAndUpdate(id,{...req.body.listing});
-
-   if(typeof req.file !=="undefined"){
-    let url=req.file.path;
-    let filename=req.file.filename;
-    Listing.image={url,filename};
-    await Listing.save();
-   }
-
-    req.flash("success","Listing is updated");
-    res.redirect(`/listings/${id}`);
-};
-
-module.exports.destroyListing=async (req,res,next)=>{
-    let {id}=req.params;
-    await listing.findByIdAndDelete(id);
-    req.flash("success","Listing deleted!");
+  if (!details) {
+    req.flash("error", "Listing you requested for does not exist!");
     res.redirect("/listings");
+  }
+
+  res.render("listings/show.ejs", { details });
+};
+
+
+
+module.exports.createListing = async (req, res, next) => {
+  try {
+    const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+    const mapToken = process.env.MAP_TOKEN;
+
+    const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
+    let response = await geocodingClient.forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1
+    }).send();
+
+    let url = req.file.path;
+    let filename = req.file.filename;
+    let newListing = new listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
+
+    // Assign geometry only if valid
+    newListing.geometry = response.body.features[0].geometry;
+
+    let savedListing = await newListing.save();
+    console.log(savedListing);
+
+
+
+
+    req.flash("success", "New listing is created");
+    res.redirect("/listings");
+  } catch (err) {
+    console.error(err);
+    req.flash("error", "Failed to create a new listing. Please try again.");
+    res.redirect("/listings/new");
+  }
+};
+
+
+module.exports.renderEditForm = async (req, res, next) => {
+  let { id } = req.params;
+  let details = await listing.findById(id);
+  if (!details) {
+    req.flash("errormsg", "Listing you requested for does not exist!");
+    return res.redirect("/listings");
+  }
+  originalImageUrl = details.image.url;
+  originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+  res.render("listings/edit.ejs", { details, originalImageUrl });
+};
+
+module.exports.updateListing = async (req, res, next) => {
+  if (!req.body.listing) {
+    throw new expressError(400, "Send valid data for listing");
+  }
+  let { id } = req.params;
+  let Listing = await listing.findByIdAndUpdate(id, { ...req.body.listing });
+
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    Listing.image = { url, filename };
+    await Listing.save();
+  }
+
+  req.flash("success", "Listing is updated");
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async (req, res, next) => {
+  let { id } = req.params;
+  await listing.findByIdAndDelete(id);
+  req.flash("success", "Listing deleted!");
+  res.redirect("/listings");
 };
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,21 +1,28 @@
-const express=require('express');
+const express = require('express');
 const wrapAsync = require('../utils/wrapAsync');
-const router=express.Router();
-const passport=require('passport');
+const router = express.Router();
+const passport = require('passport');
 const { saveRedirectUrl } = require('../middleware.js');
-const usercontroller=require("../controllers/user.js");
+const usercontroller = require("../controllers/user.js");
+const listing = require("../model/listing.js");
 
-router.get("/signup",usercontroller.renderSignupForm);
-
-router.post("/signup",wrapAsync(usercontroller.signup));
-
-
-router.get("/login",usercontroller.renderLoginForm);
-
-
-router.post("/login",saveRedirectUrl,passport.authenticate('local', { failureRedirect: '/login' ,failureFlash:true}),usercontroller.login);
-
-  router.get("/logout",usercontroller.logout);
+router.get("/", async (req, res) => {
+  const allListing = await listing.find({});
+  res.render("listings/index", { allListing });
+});
 
 
-module.exports=router;
+router.get("/signup", usercontroller.renderSignupForm);
+
+router.post("/signup", wrapAsync(usercontroller.signup));
+
+
+router.get("/login", usercontroller.renderLoginForm);
+
+
+router.post("/login", saveRedirectUrl, passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }), usercontroller.login);
+
+router.get("/logout", usercontroller.logout);
+
+
+module.exports = router;
